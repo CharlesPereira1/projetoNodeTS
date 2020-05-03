18321ccdac8e1d1ebed61891be0b9687564c2c7c
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-02 21:49:33 -0300

    Criando Model de agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-
-// Esta falando que o appointments é um array de Appointments do interface (TS)
+// Esta falando que o appointments é um array de Appointments do interface do models (TS)
 const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
@@ -29,11 +23,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
