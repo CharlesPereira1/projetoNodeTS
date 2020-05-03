ea060dc0fe0afbf0ae3e8e4671fe5077f03d58d0
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-02 21:11:00 -0300

    configure debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,14 @@ import { Router } from 'express';
 
 const routes = Router();
 
-// eslint-disable-next-line prettier/prettier
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack' }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+  return response.json(user);
+});
 
 export default routes;
