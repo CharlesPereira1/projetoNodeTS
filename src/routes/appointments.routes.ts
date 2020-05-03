347e23bc329653f65c1431332352142c18b1d2cc
Charles Pereira <charlesti.ptu@gmail.com>
2020-05-02 22:19:06 -0300

    Criando Repositorio Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,34 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-// Esta falando que o appointments é um array de Appointments do interface do models (TS)
-const appointments: Appointment[] = [];
+// instanciando a classe criada no repositories
+const appointmentsRepository = new AppointmentsRepository();
+
+/**
+ * Movido para o appointmentsRepository
+ // Esta falando que o appointments é um array de Appointments do interface do models (TS)
+ const appointments: Appointment[] = [];
+ */
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  /* Verifica se existem 2 datas e horas iguais */
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  /**
+   * Movido para o appointmentsRepository.findByDate
+   *Verifica se existem 2 datas e horas iguais
+   *const findAppointmentInSameDate = appointments.find(appointment =>
+   *  isEqual(parsedDate, appointment.date),
+   *);
+   */
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -23,9 +37,14 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
+  /**
+   * movido para o appointmentsRepository.create
+   *const appointment = new Appointment(provider, parsedDate);
+   *appointments.push(appointment);
+   */
 
-  appointments.push(appointment);
+  // dados trazidos no appointmentsRepository
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
